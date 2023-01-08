6f3923cfa54890bef10924a3e8b0b15a289193fe
Ziszel <the8thnoteofficial@gmail.com>
2023-01-08 08:39:16 +0000

    Updated Vector 2 methods to Vector 3

--- Assets/Scripts/GraphicsMath.cs
+++ Assets/Scripts/GraphicsMath.cs
@@ -5,51 +5,57 @@ public class GraphicsMath : MonoBehaviour
 {
 
     // return v1 + v2 (vectors MUST be of the same dimensions)
-    private Vector2 AddVector(Vector2 v1, Vector2 v2)
+    private Vector3 AddVector(Vector3 v1, Vector3 v2)
     {
         float xAddition = v1.x + v2.x;
         float yAddition = v1.y + v2.y;
-        return new Vector2(xAddition, yAddition);
+        float zAddition = v1.z + v2.z;
+        return new Vector3(xAddition, yAddition, zAddition);
     }
 
 // return v1 - v2 (anti-commutative: order is important)
-    private Vector2 SubVector(Vector2 v1, Vector2 v2)
+    private Vector3 SubVector(Vector3 v1, Vector3 v2)
     {
         float xSubtracted = v1.x - v1.x;
         float ySubtracted = v1.y - v1.y;
-        return new Vector2(xSubtracted, ySubtracted);
+        float zSubtracted = v1.z - v2.z;
+        return new Vector3(xSubtracted, ySubtracted, zSubtracted);
     }
 
-    private float GetVectorMagnitude(Vector2 v1)
+    private float GetVectorMagnitude(Vector3 v1)
     {
         float xSquared = v1.x * v1.x;
         float ySquared = v1.y * v1.y;
+        float zSquared = v1.z * v1.z;
         // dirty use of a cast use to the way built-in Math.Sqrt works (it returns a double)
-        return (float)Math.Sqrt(xSquared + ySquared); // Can we use the built in Sqrt method?
+        return (float)Math.Sqrt(xSquared + ySquared + zSquared); // Can we use the built in Sqrt method?
     }
 
-    private Vector2 NormaliseVector(Vector2 v1)
+    private Vector3 NormaliseVector(Vector3 v1)
     {
         float xNormalised = v1.x / GetVectorMagnitude(v1);
         float yNormalised = v1.y / GetVectorMagnitude(v1);
-        return new Vector2(xNormalised, yNormalised);
+        float zNormalised = v1.z / GetVectorMagnitude(v1);
+        return new Vector3(xNormalised, yNormalised, zNormalised);
     }
 
-    private Vector2 MultiplyByScalar(Vector2 v1, int scalar)
+    private Vector3 MultiplyByScalar(Vector3 v1, int scalar)
     {
         float xMultiplied = v1.x * scalar;
         float yMultiplied = v1.y * scalar;
-        return new Vector2(xMultiplied, yMultiplied);
+        float zMultiplied = v1.z * scalar;
+        return new Vector3(xMultiplied, yMultiplied, zMultiplied);
     }
 
     // return the dot product of v1 & v2
-    private float CalculateDot(Vector2 v1, Vector2 v2)
+    private float CalculateDot(Vector3 v1, Vector3 v2)
     {
         // The dot product is the projection of one vector on another
         // I can use it to work out if a vector is facing the same way as another, and if so, by how much
         float dotX = v1.x * v2.x;
         float dotY = v1.y * v2.y;
-        return dotX + dotY;
+        float dotZ = v1.z * v2.z;
+        return dotX + dotY + dotZ;
     }
 
     private Vector3 CalculateCross(Vector3 v1, Vector3 v2)
