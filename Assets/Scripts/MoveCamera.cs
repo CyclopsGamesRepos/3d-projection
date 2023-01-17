efbe3008f39637e90039cf789eb4c4739e439a63
Jim Verhaeghe <12163046+jpverhaeghe@users.noreply.github.com>
2023-01-17 13:29:22 +0000

    Added camera rotation - remove if it is too problematic.

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
+    // serialized variables for this script to be set in the Unity UI
+    [SerializeField] Transform centerObject;
     [SerializeField] float speed = 2;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float turnSpeed = 1;
 
     // Update is called once per frame
     void Update()
     {
+        // move our center position
         float xPos = (Input.GetAxis("Horizontal") * speed * Time.deltaTime);
         float yPos = (Input.GetAxis("Vertical") * speed * Time.deltaTime);
 
         transform.Translate(xPos, yPos, 0);
 
-    }
+        // roatate around the object in the center
+        if (Input.GetMouseButton(1))
+        {
+            transform.RotateAround(centerObject.position, Vector3.up, Input.GetAxis("Mouse X") * turnSpeed);
+            transform.RotateAround(centerObject.position, Vector3.left, Input.GetAxis("Mouse Y") * turnSpeed);
+        }
+
+    } // end Update
 }
